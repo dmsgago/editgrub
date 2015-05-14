99137364ff01053d886607ffbc40b4e3cc42eedf
debian <debian@wheezy>
2015-05-14 10:51:20 +0200

    Añadida la cabecera del script

--- editgrub.sh
+++ editgrub.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Muestra la cabecera del script
+echo $"
+ ______ _____ _____ _______ _____ _____  _    _ ____  
+|  ____|  __ \_   _|__   __/ ____|  __ \| |  | |  _ \ 
+| |__  | |  | || |    | | | |  __| |__) | |  | | |_) |
+|  __| | |  | || |    | | | | |_ |  _  /| |  | |  _ < 
+| |____| |__| || |_   | | | |__| | | \ \| |__| | |_) |
+|______|_____/_____|  |_|  \_____|_|  \_\\_____/|____/ 
+                                                      
+"
+
 # Muestra el menú principal
 echo -e "1. Cambiar la imagen de fondo del grub.\n2. Cambiar el tiempo de espera inicial.\n3. Cambiar la entrada marcada por defecto.\n"
